b13cdbd3ebae40bba6bcde169c53adf8747641f6
Bara <youj_lim@naver.com>
2023-10-30 16:40:15 +0900

    feat : 지도 우측에 선택 카테고리 목록 표시, 마커 클릭시 지도 이동
    1.더미데이터에 title 데이터 추가 후 지도 우측에 클릭된 카테고리의 장소 리스트를 표시함
    2.이벤트 마커 컨테이너 컴포넌트를 만들어서 기존의 맵마커를 컴포넌트화
    3.특정 마커 클릭시 해당 마커로 지도 중심점 이동하기

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -1,6 +1,7 @@
 // 카카오 지도 컴포넌트
 
-import { Map, MapMarker } from "react-kakao-maps-sdk";
+import styled from "styled-components";
+import { Map, MapMarker, useMap } from "react-kakao-maps-sdk";
 import { useEffect, useState } from "react";
 
 
@@ -19,41 +20,47 @@ export default function KakaoMap() {
 
   // 카페 마커가 표시될 좌표 배열
   const coffeePositions = [
-    { lat: 37.499590490909185, lng: 127.0263723554437 },
-    { lat: 37.499427948430814, lng: 127.02794423197847 },
-    { lat: 37.498553760499505, lng: 127.02882598822454 },
-    { lat: 37.497625593121384, lng: 127.02935713582038 },
-    { lat: 37.49646391248451, lng: 127.02675574250912 },
-    { lat: 37.49629291770947, lng: 127.02587362608637 },
-    { lat: 37.49754540521486, lng: 127.02546694890695 },
+    { title: "카페1", lat: 37.499590490909185, lng: 127.0263723554437 },
+    { title: "카페2", lat: 37.499427948430814, lng: 127.02794423197847 },
+    { title: "카페3", lat: 37.498553760499505, lng: 127.02882598822454 },
+    { title: "카페4", lat: 37.497625593121384, lng: 127.02935713582038 },
+    { title: "카페5", lat: 37.49646391248451, lng: 127.02675574250912 },
+    { title: "카페6", lat: 37.49629291770947, lng: 127.02587362608637 },
+    { title: "카페7", lat: 37.49754540521486, lng: 127.02546694890695 },
   ];
   const coffeeOrigin = { x: 10, y: 0 };
   // 편의점 마커가 표시될 좌표 배열
   const storePositions = [
-    { lat: 37.497535461505684, lng: 127.02948149502778 },
-    { lat: 37.49671536281186, lng: 127.03020491448352 },
-    { lat: 37.496201943633714, lng: 127.02959405469642 },
-    { lat: 37.49640072567703, lng: 127.02726459882308 },
-    { lat: 37.49640098874988, lng: 127.02609983175294 },
-    { lat: 37.49932849491523, lng: 127.02935780247945 },
-    { lat: 37.49996818951873, lng: 127.02943721562295 },
+    { title: "편의점1", lat: 37.497535461505684, lng: 127.02948149502778 },
+    { title: "편의점2", lat: 37.49671536281186, lng: 127.03020491448352 },
+    { title: "편의점3", lat: 37.496201943633714, lng: 127.02959405469642 },
+    { title: "편의점4", lat: 37.49640072567703, lng: 127.02726459882308 },
+    { title: "편의점5", lat: 37.49640098874988, lng: 127.02609983175294 },
+    { title: "편의점6", lat: 37.49932849491523, lng: 127.02935780247945 },
+    { title: "편의점7", lat: 37.49996818951873, lng: 127.02943721562295 },
   ];
   const storeOrigin = { x: 10, y: 36 };
 
   // 주차장 마커가 표시될 좌표 배열
   const carparkPositions = [
-    { lat: 37.49966168796031, lng: 127.03007039430118 },
-    { lat: 37.499463762912974, lng: 127.0288828824399 },
-    { lat: 37.49896834100913, lng: 127.02833986892401 },
-    { lat: 37.49893267508434, lng: 127.02673400572665 },
-    { lat: 37.49872543597439, lng: 127.02676785815386 },
-    { lat: 37.49813096097184, lng: 127.02591949495914 },
-    { lat: 37.497680616783086, lng: 127.02518427952202 },
+    { title: "주차장1", lat: 37.49966168796031, lng: 127.03007039430118 },
+    { title: "주차장2", lat: 37.499463762912974, lng: 127.0288828824399 },
+    { title: "주차장3", lat: 37.49896834100913, lng: 127.02833986892401 },
+    { title: "주차장4", lat: 37.49893267508434, lng: 127.02673400572665 },
+    { title: "주차장5", lat: 37.49872543597439, lng: 127.02676785815386 },
+    { title: "주차장6", lat: 37.49813096097184, lng: 127.02591949495914 },
+    { title: "주차장7", lat: 37.497680616783086, lng: 127.02518427952202 },
   ];
   const carparkOrigin = { x: 10, y: 72 };
 
   // 마커 카테고리 선택
-  const [selectedCategory, setSelectedCategory] = useState("coffee");
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  // 클릭한 마커
+  const [selectedMarker, setSelectedMarker] = useState();
+
+  // 인포 윈도우 오픈 여부 설정
+  const [isOpen, SetIsOpen] = useState(false);
 
 
 
@@ -137,6 +144,50 @@ export default function KakaoMap() {
     }));
 	};
 
+  // 특정 카테고리 클릭시
+  const handleClickCategory = (category)=>{
+    console.log("카테고리 클릭", category);
+
+    // 카테고리 지정
+    setSelectedCategory(category);
+  };
+
+  // 특정 마커 클릭 이벤트
+  const handleClickMarker = (map, marker, index)=>{
+    console.log("클릭함", index);
+    // 클릭된 마커로 지도 중심점 이동하기
+    map.panTo(marker.getPosition());
+  };
+
+
+  // 이벤트가 걸린 마커 컨테이너 컴포넌트
+  const EventMarkerContainer = ({ index, position, spriteCoord })=>{
+    // 클릭한 마커로의 지도 이동을 위해 map객체 가져오기
+    const map = useMap();
+
+    return(
+      <MapMarker
+        // 지도 상 마커를 표시할 위치
+        position={{ lat: position.lat, lng: position.lng }}
+        // 마커 이미지
+        image={{
+          // 이미지 src
+          src: markerImageSrc,
+          // 이미지 사이즈
+          size: imageSize,
+          // 스프라이트 이미지일 경우 옵션으로 위치 조정
+          options: {
+            // 스프라이트 이미지의 사이즈
+            spriteSize: spriteSize,
+            // 스프라이트 이미지의 좌표값
+            spriteOrigin: spriteCoord,
+          },
+        }}
+        // 해당 마커 클릭시 이벤트 발생
+        onClick={(marker)=> handleClickMarker(map, marker, index) }
+      ></MapMarker>
+    )
+  }; // 이벤트가 걸린 마커 컨테이너 컴포넌트
 
   return (
     <>
@@ -157,75 +208,81 @@ export default function KakaoMap() {
       >
         {/* 지도 위 마커 */}
         {selectedCategory === "coffee" &&
-        coffeePositions.map((position) => (
-          <MapMarker
-            key={`coffee-${position.lat},${position.lng}`}
-            // 지도 상 마커를 표시할 위치
-            position={position}
-            // 마커 이미지
-            image={{
-              // 이미지 src
-              src: markerImageSrc,
-              // 이미지 사이즈
-              size: imageSize,
-              // 스프라이트 이미지일 경우 옵션으로 위치 조정
-              options: {
-                // 스프라이트 이미지의 사이즈
-                spriteSize: spriteSize,
-                // 스프라이트 이미지의 좌표값
-                spriteOrigin: coffeeOrigin,
-              },
-            }}
-          />
+        coffeePositions.map((position, index) => (
+          <EventMarkerContainer
+          index={index}
+          key={`EventMarkerContainer-${position.lat},${position.lng}`}
+          position={{ lat: position.lat, lng: position.lng }}
+          spriteCoord={coffeeOrigin} />
         ))}
         {selectedCategory === "store" &&
-        storePositions.map((position) => (
-          <MapMarker
-            key={`store-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markerImageSrc,
-              size: imageSize,
-              options: {
-                spriteSize: spriteSize,
-                spriteOrigin: storeOrigin,
-              },
-            }}
-          />
+        storePositions.map((position, index) => (
+          <EventMarkerContainer
+          index={index}
+          key={`EventMarkerContainer-${position.lat},${position.lng}`}
+          position={{ lat: position.lat, lng: position.lng }}
+          spriteCoord={storeOrigin} />
         ))}
+        
         {selectedCategory === "carpark" &&
-        carparkPositions.map((position) => (
-          <MapMarker
-            key={`carpark-${position.lat},${position.lng}`}
-            position={position}
-            image={{
-              src: markerImageSrc,
-              size: imageSize,
-              options: {
-                spriteSize: spriteSize,
-                spriteOrigin: carparkOrigin,
-              },
-            }}
-          />
+        carparkPositions.map((position, index) => (
+          <EventMarkerContainer
+          index={index}
+          key={`EventMarkerContainer-${position.lat},${position.lng}`}
+          position={{ lat: position.lat, lng: position.lng }}
+          spriteCoord={carparkOrigin} />
         ))}
       </Map>
       {/* 지도 위에 표시될 마커 카테고리 */}
       <div className="category">
         <ul>
-          <li id="coffeeMenu" onClick={() => setSelectedCategory("coffee")}>
+          <li id="coffeeMenu" onClick={() => handleClickCategory("coffee")}>
             <span className="ico_comm ico_coffee"></span>
             커피숍
           </li>
-          <li id="storeMenu" onClick={() => setSelectedCategory("store")}>
+          <li id="storeMenu" onClick={() => handleClickCategory("store")}>
             <span className="ico_comm ico_store"></span>
             편의점
           </li>
-          <li id="carparkMenu" onClick={() => setSelectedCategory("carpark")}>
+          <li id="carparkMenu" onClick={() => handleClickCategory("carpark")}>
             <span className="ico_comm ico_carpark"></span>
             주차장
           </li>
         </ul>
       </div>
+      {/* 지도 오른쪽 마커 장소 표시 */}
+      <PlaceListBox>
+        <h2>선택한 종류에 맞는 마커 리스트 표시</h2>
+        {selectedCategory === "coffee" &&
+        coffeePositions.map((position, index) => {
+          return(
+            <div key={index}>{position.title}</div>
+          )
+        })}
+        {selectedCategory === "store" &&
+        storePositions.map((position, index) => {
+          return(
+            <div key={index}>{position.title}</div>
+          )
+        })}
+        {selectedCategory === "carpark" &&
+        carparkPositions.map((position, index) => {
+          return(
+            <div key={index}>{position.title}</div>
+          )
+        })}
+      </PlaceListBox>
     </>
   )
 }
+
+const PlaceListBox = styled.div`
+  background-color: #ff9dd1;
+  width: 300px;
+  height: calc(100% - 20px);
+  position: absolute;
+  top: 20px;
+  right: 0;
+  z-index: 10;
+  overflow-y: auto;
+`;
